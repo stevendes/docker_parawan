647cf368b4a02a8cef447cb253ea3bddae21c104
Steven Desvars <steven.desvars@gmail.com>
2020-12-10 12:25:18 -0300

    Add docker-compose file

--- docker_run.sh
+++ docker_run.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-IMAGE=padawan
+xhost +local:docker
 
-docker run -it \
-  --rm \
-  --network=host \
-  -e DISPLAY \
-  -e QT_X11_NO_MITSHM=1 \
-  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  -v "$HOME/.Xauthority:/root/.Xauthority:rw" \
-  -v "/etc/localtime:/etc/localtime:ro" \
-  -v "/dev/input:/dev/input" \
-  --gpus all \
-  -v "$PWD:/home/" \
-  padawan
+docker-compose run padawan
+docker-compose stop padawan
